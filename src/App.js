9e925d13bdd7c9423024af3fdc7f51a2cfc8f665
vishalnotfound <ping4vishal@gmail.com>
2025-11-02 22:15:38 +0530

    added stuffs

--- src/App.js
+++ src/App.js
@@ -1,1 +1,13 @@
-const count=0;
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-indigo-600 to-purple-700 text-white">
+      <h1 className="text-5xl font-extrabold mb-4">Tailwind is Working 🚀</h1>
+      <p className="text-10xl opacity-80">If you see a purple gradient background</p>
+    </div>
+  );
+}
+
+export default App;
+
