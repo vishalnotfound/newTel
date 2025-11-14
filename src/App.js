f65794a058f02c5086d2f6a0fccd719f5a09b505
vishalnotfound <ping4vishal@gmail.com>
2025-11-14 20:53:13 +0530

    test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-indigo-600 to-purple-700 text-white">
-     
+      <h1 className="text-4xl font-bold mb-4">Welcome to NewTel</h1>
     </div>
   );
 }
