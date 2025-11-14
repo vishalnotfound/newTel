d4d63df5e8dc07c61532e68f77e1dad153326600
vishalnotfound <ping4vishal@gmail.com>
2025-11-14 20:54:10 +0530

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-indigo-600 to-purple-700 text-white">
       <h1 className="text-4xl font-bold mb-4">Welcome to NewTel</h1>
-    </div>
+      <p className="text-lg">Your gateway to seamless communication.</p>
+    </div>    
   );
 }
 
