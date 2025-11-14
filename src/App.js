a958b2511dfaf1adfa1d79f750accc032454a193
vishalnotfound <ping4vishal@gmail.com>
2025-11-14 20:52:57 +0530

    test

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-indigo-600 to-purple-700 text-white">
-      <h1 className="text-5xl font-extrabold mb-4">Tailwind is Working 🚀</h1>
-      
+     
     </div>
   );
 }
